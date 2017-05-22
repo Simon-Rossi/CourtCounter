d3aa16445d8e6b2fe9d71da625be09136e3d611c
Simon-Rossi <simorossipro@gmail.com>
2017-05-22 14:17:10 +0200

    CourtCounter

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -5,33 +5,47 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+
 public class MainActivity extends AppCompatActivity {
 
     int teamAScore = 0;
+    int teamBScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         displayForTeamA(0);
+        displayForTeamB(0);
+        whoIsWinning();
     }
 
     /**
      * Used to increment the score of team A
      */
-    public void freeThrow(View view) {
+    public void freeThrowA(View view) {
         teamAScore = teamAScore + 1;
         displayForTeamA(teamAScore);
+        whoIsWinning();
     }
 
-    public void twoPoints(View view) {
+    public void twoPointsA(View view) {
         teamAScore = teamAScore + 2;
         displayForTeamA(teamAScore);
+        whoIsWinning();
     }
 
-    public void threePoints(View view) {
+    public void threePointsA(View view) {
         teamAScore = teamAScore + 3;
         displayForTeamA(teamAScore);
+        whoIsWinning();
+    }
+
+    public void resetPointsA(View view) {
+        teamAScore = 0;
+        displayForTeamA(teamAScore);
+        whoIsWinning();
     }
 
     /**
@@ -41,4 +55,59 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
+
+
+    // ==========> TEAM B <============
+
+    /**
+     * Used to increment the score of team A
+     */
+
+    public void freeThrowB(View view) {
+        teamBScore = teamBScore + 1;
+        displayForTeamB(teamBScore);
+        whoIsWinning();
+    }
+
+    public void twoPointsB(View view) {
+        teamBScore = teamBScore + 2;
+        displayForTeamB(teamBScore);
+        whoIsWinning();
+    }
+
+    public void threePointsB(View view) {
+        teamBScore = teamBScore + 3;
+        displayForTeamB(teamBScore);
+        whoIsWinning();
+    }
+
+    public void resetPointsB(View view) {
+        teamBScore = 0;
+        displayForTeamB(teamBScore);
+        whoIsWinning();
+    }
+
+
+     // Displays the given score for Team B.
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void whoIsWinning(){
+
+        if(teamAScore > teamBScore){
+            TextView winnerView = (TextView) findViewById(R.id.winner_text);
+            winnerView.setText(String.valueOf("    Team A\nis winning!"));
+        }else if(teamAScore < teamBScore){
+            TextView winnerView = (TextView) findViewById(R.id.winner_text);
+            winnerView.setText(String.valueOf("    Team B\nis winning!"));
+        }else{
+            TextView winnerView = (TextView) findViewById(R.id.winner_text);
+            winnerView.setText(String.valueOf("Draw"));
+        }
+    } // Winner result in text view
 }
+
+
+
